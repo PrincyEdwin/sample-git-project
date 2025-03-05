45ffb4b528f6d26d05d77e5e474f3af5bb88ea09
PrincyEdwin <rmahibaprincy@gmail.com>
2025-03-05 17:42:57 -0600

    Star program using nested for loops!

---
+++ src/main/java/org/gitsample/Star.java
@@ -0,0 +1,17 @@
+package org.gitsample;
+
+public class Star {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=5; i=i+1)
+		{
+			for(int j=1 ; j<=1*i ; j=j+1)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
